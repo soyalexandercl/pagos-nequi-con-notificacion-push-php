7f3f6fb9347a765e4605c97a83fe4becdf806eaa
Jhon Alexander <jcl0040@alu.medac.es>
2026-02-03 14:24:03 +0100

    Instalar la librería para leer el .env

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,15 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 use App\Servicios\ServicioPago;
 
 $servicio = new ServicioPago();
-$servicio->solicitarPagoPush("3235230270", 1000, "Cubitx");
+
+$respuesta = $servicio->solicitarPagoPush("3235230270", 1000, "Cubitx");
+
+echo "<pre>";
+var_dump($respuesta);
+echo "</pre>";
